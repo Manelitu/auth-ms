395a8a3a435571321b0dd0494723a99bf4146648
Manelito <emanuelwork1@gmail.com>
2023-08-16 21:30:52 -0300

    feat: adding configs on main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  configCors,
+  configSwagger,
+  configClassValidator,
+  PrismaHelper,
+} from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const db = app.get(PrismaHelper);
+  await db.enableShutdownHooks(app);
+
+  configCors(app);
+  configClassValidator(app);
+  configSwagger({
+    app: app,
+    config: {
+      url: '/docs',
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
